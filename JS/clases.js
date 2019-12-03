ebada501557d370308ffe5ccff72f7cb7e354d5f
ElvisUceda <47511391+ElvisUceda@users.noreply.github.com>
2019-12-02 21:24:30 -0500

    commit JS

---
+++ JS/clases.js
@@ -0,0 +1,85 @@
+//var respuesta=confirm('Desea salir?');
+//alert('La respuesta: '+ respuesta);
+
+/*var n1=10;
+var n2=20;
+var resp= n1>n2;
+
+alert('La respuesta es:' + resp);*/
+
+/*var edad = prompt('Cuantos años tienes');
+if (edad >=25){
+	alert('Usted tiene 25 años o mas');
+	
+	}else if (edad==24){
+	alert('Usted tiene 24 años');
+	}else if (edad>=13 && edad<24){
+		alert('Usted tiene entre 13 y 23 años');
+	}else {
+		alert('Usted tiene 12 años o menos');
+	}*/
+	
+	
+	/*var edad = prompt('Que edad tienes?');
+	var nombre;
+	if (edad >= 18){
+		nombre=prompt('Cual es tu nombre?');
+		if (nombre=='Jimena'){
+			alert('Hola ' + nombre);
+		}else{
+			alert('Usted es mayor de edad, pero no es ' + nombre);
+		}
+	}else{
+		alert('Usted es menor de edad, hasta pronto');
+	}*/
+	
+//	var f = new Date();
+//	var dia = f.getDay();
+//	alert(dia); 
+
+/*switch(dia){
+	case 0:
+	alert('Hoy es domingo');
+	break;
+	case 1:
+	alert('Hoy es lunes');
+	break;
+	case 2:
+	alert('Hoy es martes');
+	break;
+	case 3:
+	alert('Hoy es miercoles');
+	break;
+	case 4:
+	alert('Hoy es jueves');
+	break;
+	case 5:
+	alert('Hoy es viernes');
+	break;
+	default:
+	alert('Hoy es sabado');
+	break;
+} */
+
+/*var titulo = document.getElementById('titulo');
+titulo.innerHTML='Nuevo titulo';
+alert(titulo.innerHTML);*/
+
+/*var titulo=document.querySelector('h1');
+alert(titulo.innerHTML);
+var titulo2=document.querySelector('#titulo');
+alert(titulo2.innerHTML);
+var titulo3=document.getElementById('titulo');
+alert(titulo3.innerHTML);
+*/
+
+var nombre = prompt('Ingrese su nombre:');
+if (nombre=='Elvis'){
+	alert('Bienvenido ' + nombre);
+}
+else if (nombre!='Elvis'){
+	alert('Usuario incorrecto');
+}
+
+	
+	
